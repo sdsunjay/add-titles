60086d03ac6e66802b8dfafdb7f138f5dadd84da
Sunjay <sdsunjay73@yahoo.com>
2018-09-11 01:41:18 -0700

    added created_at and updated_at fields, tested and confirmed working

--- parse.py
+++ parse.py
@@ -1,11 +1,14 @@
 import config
 import tmdbsimple as tmdb
 import psycopg2
+from datetime import datetime
+
 
 def database():
 
-    #Define our connection string
-    conn_string = "host='localhost' dbname='{}' user='{}' password = '{}'".format(config.DB_NAME, config.USER, config.PASSWORD)
+    # Define our connection string
+    conn_string = "host='localhost' dbname='{}' user='{}' password = '{}'".format(
+        config.DB_NAME, config.USER, config.PASSWORD)
     # print the connection string we will use to connect
     print("Connecting to database\n	->%s" % (conn_string))
 
@@ -17,19 +20,26 @@ def database():
     print("Connected!\n")
     return conn
 
-def handle_genres(cur, movie_id, genre_ids):
+
+def handle_genres(cur, movie_id, genre_ids, dt):
 
     # insert movie id and each genre id into categorization table
     for genre_id in genre_ids:
-        cur.execute("INSERT INTO categorizations(movie_id, genre_id) VALUES (%s, %s)", (movie_id, genre_id))
+        cur.execute(
+            "INSERT INTO categorizations(movie_id, genre_id, created_at, updated_at) VALUES (%s, %s, %s, %s)", (movie_id, genre_id, dt, dt))
 
-def handle_movie(conn, cur, movie):
+
+def handle_movie(conn, cur, movie, dt):
     user_id = 1
-    cur.execute("INSERT INTO movies(id, user_id, vote_count, vote_average, title, popularity, poster_path, original_language, backdrop_path, adult, overview, release_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ", (movie['id'], user_id, movie['vote_count'],  movie['vote_average'], movie['title'], movie['popularity'], movie['poster_path'], movie['original_language'], movie['backdrop_path'], movie['adult'], movie['overview'], movie['release_date']))
-    handle_genres(cur, movie['id'], movie['genre_ids'])
+    # if None not in movie.values():
+    cur.execute("INSERT INTO movies(id, user_id, vote_count, vote_average, title, popularity, poster_path, original_language, backdrop_path, adult, overview, release_date, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ", (movie['id'], user_id, movie['vote_count'], movie['vote_average'], movie['title'], movie['popularity'], movie['poster_path'], movie['original_language'], movie['backdrop_path'], movie['adult'], movie['overview'], movie['release_date'], dt, dt))
+    handle_genres(cur, movie['id'], movie['genre_ids'], dt)
 
-def handle_genre(conn, cur, genre):
-    cur.execute("INSERT INTO genres(id, name) VALUES (%s, %s)", (genre['id'], genre['name']))
+
+def handle_genre(conn, cur, genre, dt):
+    cur.execute("INSERT INTO genres(id, name, created_at, updated_at) VALUES (%s, %s, %s, %s)",
+                (genre['id'], genre['name'], dt, dt))
+
 
 def create_table(conn):
     cur = conn.cursor()
@@ -64,11 +74,15 @@ def create_table(conn):
     cur.close()
 
 # For testing
+
+
 def test_main():
 
-    genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]
+    genres = [{"id": 28, "name": "Action"}, {"id": 12, "name": "Adventure"}, {"id": 16, "name": "Animation"}, {"id": 35, "name": "Comedy"}, {"id": 80, "name": "Crime"}, {"id": 99, "name": "Documentary"}, {"id": 18, "name": "Drama"}, {"id": 10751, "name": "Family"}, {"id": 14, "name": "Fantasy"}, {"id": 36, "name": "History"}, {
+        "id": 27, "name": "Horror"}, {"id": 10402, "name": "Music"}, {"id": 9648, "name": "Mystery"}, {"id": 10749, "name": "Romance"}, {"id": 878, "name": "Science Fiction"}, {"id": 10770, "name": "TV Movie"}, {"id": 53, "name": "Thriller"}, {"id": 10752, "name": "War"}, {"id": 37, "name": "Western"}]
 
-    movies = [{'vote_count': 1337, 'id': 353081, 'video': False, 'vote_average': 7.4, 'title': 'Mission: Impossible - Fallout', 'popularity': 121.388, 'poster_path': '/AkJQpZp9WoNdj7pLYSj1L0RcMMN.jpg', 'original_language': 'en', 'original_title': 'Mission: Impossible - Fallout', 'genre_ids': [12, 28, 53], 'backdrop_path': '/5qxePyMYDisLe8rJiBYX8HKEyv2.jpg', 'adult': False, 'overview': 'When an IMF mission ends badly, the world is faced with dire consequences. As Ethan Hunt takes it upon himself to fulfil his original briefing, the CIA begin to question his loyalty and his motives. The IMF team find themselves in a race against time, hunted by assassins while trying to prevent a global catastrophe.', 'release_date': '2018-07-25'}]
+    movies = [{'vote_count': 1337, 'id': 353081, 'video': False, 'vote_average': 7.4, 'title': 'Mission: Impossible - Fallout', 'popularity': 121.388, 'poster_path': '/AkJQpZp9WoNdj7pLYSj1L0RcMMN.jpg', 'original_language': 'en', 'original_title': 'Mission: Impossible - Fallout', 'genre_ids': [12, 28, 53], 'backdrop_path': '/5qxePyMYDisLe8rJiBYX8HKEyv2.jpg', 'adult': False,
+               'overview': 'When an IMF mission ends badly, the world is faced with dire consequences. As Ethan Hunt takes it upon himself to fulfil his original briefing, the CIA begin to question his loyalty and his motives. The IMF team find themselves in a race against time, hunted by assassins while trying to prevent a global catastrophe.', 'release_date': '2018-07-25'}]
 
     conn = database()
     create_table(conn)
@@ -97,20 +111,25 @@ def main():
     conn = database()
     # create_table(conn)
     # Open a cursor to perform database operations
-    # genre_cur = conn.cursor()
-    # for genre in genres:
-    #    handle_genre(conn, genre_cur, genre)
+    genre_cur = conn.cursor()
+    genres = [{"id": 28, "name": "Action"}, {"id": 12, "name": "Adventure"}, {"id": 16, "name": "Animation"}, {"id": 35, "name": "Comedy"}, {"id": 80, "name": "Crime"}, {"id": 99, "name": "Documentary"}, {"id": 18, "name": "Drama"}, {"id": 10751, "name": "Family"}, {"id": 14, "name": "Fantasy"}, {"id": 36, "name": "History"}, {
+        "id": 27, "name": "Horror"}, {"id": 10402, "name": "Music"}, {"id": 9648, "name": "Mystery"}, {"id": 10749, "name": "Romance"}, {"id": 878, "name": "Science Fiction"}, {"id": 10770, "name": "TV Movie"}, {"id": 53, "name": "Thriller"}, {"id": 10752, "name": "War"}, {"id": 37, "name": "Western"}]
 
-    # genre_cur.close()
+    dt = datetime.now()
+    for genre in genres:
+        handle_genre(conn, genre_cur, genre, dt)
+
+    genre_cur.close()
     # Open a cursor to perform database operations
     movie_cur = conn.cursor()
 
     movies = tmdb.Movies()
     # TODO (Sunjay) make the page rage nonstatic
-    for page_number in range(1, 3):
-        dict_of_movies = movies.popular(**{'page' :  page_number})
+    dt = datetime.now()
+    for page_number in range(1, 5):
+        dict_of_movies = movies.popular(**{'page':  page_number})
         for movie in dict_of_movies['results']:
-            handle_movie(conn, movie_cur, movie)
+            handle_movie(conn, movie_cur, movie, dt)
 
     # Make the changes to the database persistent
     conn.commit()
@@ -118,6 +137,7 @@ def main():
     # Close communication with the database
     movie_cur.close()
     conn.close()
+
 
 if __name__ == '__main__':
     main()
